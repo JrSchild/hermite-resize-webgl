08ea58248ed164a25804cbbc7df99f986a33362b
Joram Ruitenschild <joram.ruitenschild@gmail.com>
2016-01-10 16:17:39 +0100

    Remove stuff

--- src/set-value.js
+++
@@ -1,7 +0,0 @@
-setValue(method, location, ...values) {
-  this.gl[method].apply(this.gl[method], [this.gl.getUniformLocation(this.program), ...values]);
-}
-
-setValue('uniform2f', 'time', val1, val2);
-
-// gl.uniform2f(gl.getUniformLocation(currentProgram, 'time'), val, val2);
